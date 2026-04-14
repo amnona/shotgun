f617c9ad3acc21d275665d6fc9c14607c2fdbc2c
amnona <amnonim@gmail.com>
2026-04-14 15:48:42 +0300

    add rarefaction and type

--- process_sra_table.py
+++ process_sra_table.py
@@ -18,7 +18,7 @@ class TrackAction(argparse.Action):
         setattr(namespace, 'provided_args', provided)
 
 
-def run_pipeline_on_sra_table(inputname, parallel=True, num_parallel=4, pipeline_script='~/git/shotgun/shotgun_pipeline.py', skip_if_exists=True, start_step=0, database='~/databases/uniref/db-uniref50.dmnd', sensitivity='fast', iterate=False, threads='10'):
+def run_pipeline_on_sra_table(inputname, parallel=True, num_parallel=4, pipeline_script='~/git/shotgun/shotgun_pipeline.py', skip_if_exists=True, start_step=0, database='~/databases/uniref/db-uniref50.dmnd', sensitivity='fast', iterate=False, threads='10', depth=None, tmp_dir=None, type=None):
         '''Run the sample pipeline on all samples listed in the SRA metadata table
         
         Parameters
@@ -43,6 +43,13 @@ def run_pipeline_on_sra_table(inputname, parallel=True, num_parallel=4, pipeline
                 whether to use the --iterate flag for diamond alignment (for iterative searches)
         threads: str, optional
                 number of threads to use for diamond alignment
+        depth: int, optional
+                if set, rarify each sample to this depth before alignment
+        tmp_dir: str, optional
+                path to temporary directory to use for DIAMOND (if not set, will use current directory)
+        type: str, optional
+                if "uniref50" or "uniref90" use relevant defaults (iterate, sensitivity, database) unless those parameters were explicitly provided by the user
+                None (default): use provided or default parameters for all settings
         '''
         pipeline_script = os.path.expanduser(pipeline_script)
         logger.info(f"Running pipeline on SRA table {inputname} with parallel={parallel}")
@@ -75,7 +82,7 @@ def run_pipeline_on_sra_table(inputname, parallel=True, num_parallel=4, pipeline
                 samples.append(csamp)
 
         # Build the command to run for each sample
-        base_cmd = [sys.executable, pipeline_script, '--start-step', str(start_step), '--database', database, '--sensitivity', sensitivity, '--threads', threads]
+        base_cmd = [sys.executable, pipeline_script, '--start-step', str(start_step), '--database', database, '--sensitivity', sensitivity, '--threads', threads, '--tmp-dir', tmp_dir, '--depth', str(depth)]
         if skip_if_exists:
                 base_cmd += ['--skip-if-exists']
         if iterate:
@@ -125,6 +132,8 @@ def main(argv):
     parser.add_argument('--iterate', action='store_true', help='Iterate over split files during alignment', default=False)
     parser.add_argument('--type', type=str, help='if "uniref50" or "uniref90" use relevant defaults (iterate, sensitivity, database)', default=None)
     parser.add_argument('--threads', type=str, help='Number of threads to use for each sample pipeline', default='5')
+    parser.add_argument('--depth', type=int, help='Rarification depth for each sample', default=None)
+    parser.add_argument('--tmp-dir', type=str, help='Temporary directory to use for DIAMOND', default=None)
 
     args = parser.parse_args(sys.argv[1:])
     # add file logging
@@ -147,7 +156,7 @@ def main(argv):
             logger.warning(f"Unknown type {args.type}, using provided or default parameters")
 
 
-    run_pipeline_on_sra_table(args.input, parallel=args.parallel, num_parallel=args.num_parallel, skip_if_exists=args.skip_if_exists, start_step=args.start_step, pipeline_script=args.pipeline_script, database=args.database, sensitivity=args.sensitivity, iterate=args.iterate, threads=args.threads)
+    run_pipeline_on_sra_table(args.input, parallel=args.parallel, num_parallel=args.num_parallel, skip_if_exists=args.skip_if_exists, start_step=args.start_step, pipeline_script=args.pipeline_script, database=args.database, sensitivity=args.sensitivity, iterate=args.iterate, threads=args.threads, depth=args.depth, tmp_dir=args.tmp_dir, type=args.type)
     logger.info("Shotgun pipeline finished")
 
 
